c464c816198e8781b313dcf82f51c0e76f8d077a
euramonsantos19-prog <euramonsantos19@gmail.com>
2026-03-12 20:43:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,59 @@
-ofertas = df[
-    (df["programa"] == programa) &
-    (df["cpf"] >= cpf_necessario) &
-    (df["média por cpf"] <= media_limite)
-]
+import streamlit as st
+import pandas as pd
 
-ofertas = ofertas.sort_values(by="custo do milheiro")
+st.title("Next Gate Milhas Online")
 
-milhas_restantes = milhas_desejadas
-combinacao = []
+uploaded_file = st.file_uploader("Upload da base de milhas", type=["xlsx"])
 
-for _, row in ofertas.iterrows():
+if uploaded_file:
 
-    if milhas_restantes <= 0:
-        break
+    df = pd.read_excel(uploaded_file)
 
-    milhas_disponiveis = row["quantidade"]
+    # padronizar colunas
+    df.columns = df.columns.str.strip().str.lower()
 
-    milhas_usadas = min(milhas_disponiveis, milhas_restantes)
+    # converter valores tipo 24K
+    def converter_k(valor):
+        if isinstance(valor, str):
+            valor = valor.replace("K","000").replace("k","000")
+        return valor
 
-    nova_linha = row.copy()
-    nova_linha["milhas_usadas"] = milhas_usadas
+    df["média por cpf"] = df["média por cpf"].apply(converter_k)
 
-    combinacao.append(nova_linha)
+    df["quantidade"] = pd.to_numeric(df["quantidade"], errors="coerce")
+    df["cpf"] = pd.to_numeric(df["cpf"], errors="coerce")
+    df["média por cpf"] = pd.to_numeric(df["média por cpf"], errors="coerce")
+    df["custo do milheiro"] = pd.to_numeric(df["custo do milheiro"], errors="coerce")
 
-    milhas_restantes -= milhas_usadas
+    programa = st.selectbox("Programa", df["programa"].dropna().unique())
 
+    milhas_desejadas = st.number_input("Milhas desejadas", min_value=0)
 
-resultado = pd.DataFrame(combinacao)
+    cpf_necessario = st.number_input("CPF necessários", min_value=0)
 
-if milhas_restantes > 0:
+    media_limite = st.number_input("Média por CPF", min_value=0)
 
-    st.warning("Não foi possível atingir a quantidade desejada.")
+    if st.button("Encontrar melhor combinação"):
 
-else:
+        ofertas = df[
+            (df["programa"] == programa) &
+            (df["cpf"] >= cpf_necessario) &
+            (df["média por cpf"] <= media_limite)
+        ]
 
-    st.subheader("Melhor combinação encontrada")
-    st.data
+        ofertas = ofertas.sort_values(by="custo do milheiro")
+
+        milhas_restantes = milhas_desejadas
+        combinacao = []
+
+        for _, row in ofertas.iterrows():
+
+            if milhas_restantes <= 0:
+                break
+
+            milhas_disponiveis = row["quantidade"]
+
+            milhas_usadas = min(milhas_disponiveis, milhas_restantes)
+
+            nova_linha = row.copy()
+            nova_linha["milhas_us
